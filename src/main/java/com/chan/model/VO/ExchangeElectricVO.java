a92f47d04be42328802c7477d49a8390b6675ac7
zhucheng <Posture_Cc@163.com>
2020-08-01 18:10:06 +0800

    update

--- src/main/java/com/chan/model/VO/ExchangeElectricVO.java
+++ src/main/java/com/chan/model/VO/ExchangeElectricVO.java
@@ -11,19 +11,19 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.io.Serializable;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+//@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
 
 //@Document 文档对象 （索引信息、文档类型 ）
-@Document(indexName = ExchangeElectricVO.EXCHANGE_ELECTRIC)
+@Document(indexName = ExchangeElectricVO.EXCHANGE_ELECTRIC, shards = 1, replicas = 0)
 public class ExchangeElectricVO implements Serializable {
 
     /**
      * 索引名称只能小写!
      */
-    public static final String EXCHANGE_ELECTRIC = "exchange_electric";
+    public static final String EXCHANGE_ELECTRIC = "exchange_electric_rest";
 
     @Id
     //@Id 文档主键 唯一标识
@@ -61,4 +61,91 @@ public class ExchangeElectricVO implements Serializable {
     @Field(index = true, store = true, type = FieldType.Date)
     private String createTime;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getOldBat() {
+        return oldBat;
+    }
+
+    public void setOldBat(String oldBat) {
+        this.oldBat = oldBat;
+    }
+
+    public String getNewBat() {
+        return newBat;
+    }
+
+    public void setNewBat(String newBat) {
+        this.newBat = newBat;
+    }
+
+    public Long getOperatorId() {
+        return operatorId;
+    }
+
+    public void setOperatorId(Long operatorId) {
+        this.operatorId = operatorId;
+    }
+
+    public String getOperatorName() {
+        return operatorName;
+    }
+
+    public void setOperatorName(String operatorName) {
+        this.operatorName = operatorName;
+    }
+
+    public String getCabinetName() {
+        return cabinetName;
+    }
+
+    public void setCabinetName(String cabinetName) {
+        this.cabinetName = cabinetName;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
 }
---
+++ src/main/java/com/chan/repo/ExchangeElectricRepository.java
@@ -0,0 +1,10 @@
+package com.chan.repo;
+
+import com.chan.model.VO.ExchangeElectricVO;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+
+public interface ExchangeElectricRepository extends ElasticsearchRepository<ExchangeElectricVO, Object> {
+
+
+}
