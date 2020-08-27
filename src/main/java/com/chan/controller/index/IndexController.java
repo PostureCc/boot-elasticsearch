985f1a204280bcad2ce496ea4651c518108982f2
zhucheng <Posture_Cc@163.com>
2020-08-27 11:02:26 +0800

    update

--- src/main/java/com/chan/controller/index/IndexController.java
+++ src/main/java/com/chan/controller/index/IndexController.java
@@ -1,23 +1,48 @@
-//package com.chan.controller.index;
-//
-//import com.chan.model.VO.ExchangeElectricVO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class IndexController {
-//
-//    @Autowired
-//    private ElasticSearchUtils elasticSearchUtils;
-//
-//    @GetMapping("/createIndex")
-//    public void createIndex() {
-//        String indexName = ExchangeElectricVO.EXCHANGE_ELECTRIC,
-//                settings = "{\"index\":{\"number_of_shards\":1,\"number_of_replicas\":0}}",
-//                mapping = "{\"properties\":{\"cabinetName\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"clientId\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"createTime\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"id\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"mobile\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"newBat\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"oldBat\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"operatorId\":{\"type\":\"long\"},\"operatorName\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"realName\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}},\"uid\":{\"type\":\"text\",\"fields\":{\"keyword\":{\"type\":\"keyword\",\"ignore_above\":256}}}}}";
-//
-//        elasticSearchUtils.createIndex(indexName, settings, mapping);
-//    }
-//
-//}
+package com.chan.controller.index;
+
+import com.chan.config.ElasticSearchConfig;
+import com.chan.model.VO.ExchangeElectricVO;
+import lombok.extern.log4j.Log4j2;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@Log4j2
+@RestController
+public class IndexController {
+
+
+    @GetMapping("/createIndex")
+    public void createIndex() throws IOException {
+        String indexName = ExchangeElectricVO.EXCHANGE_ELECTRIC,
+                settings = "{\"index\":{\"number_of_shards\":1,\"number_of_replicas\":0}}",
+                mapping = "{\"properties\":{\"cabinetId\":{\"type\":\"keyword\"},\"cabinetName\":{\"type\":\"keyword\"},\"clientId\":{\"type\":\"keyword\"},\"createTime\":{\"type\":\"date\",\"format\":\"yyyy-MM-dd HH:mm:ss\"},\"id\":{\"type\":\"keyword\"},\"mobile\":{\"type\":\"keyword\"},\"newBat\":{\"type\":\"keyword\"},\"oldBat\":{\"type\":\"keyword\"},\"operatorId\":{\"type\":\"long\"},\"operatorName\":{\"type\":\"keyword\"},\"realName\":{\"type\":\"keyword\"},\"uid\":{\"type\":\"keyword\"}}}";
+
+        CreateIndexRequest request = new CreateIndexRequest(indexName);
+        request.settings(settings, XContentType.JSON);
+        request.mapping(mapping, XContentType.JSON);
+
+        CreateIndexResponse response = ElasticSearchConfig.getRestHighLevelClient().indices().create(request, RequestOptions.DEFAULT);
+
+        log.info("response {}", response);
+    }
+
+    @GetMapping("/deleteIndex")
+    public void deleteIndex() {
+        DeleteIndexRequest request = new DeleteIndexRequest(ExchangeElectricVO.EXCHANGE_ELECTRIC);
+        try {
+            AcknowledgedResponse response = ElasticSearchConfig.getRestHighLevelClient().indices().delete(request, RequestOptions.DEFAULT);
+            log.info("response {}", response.isAcknowledged());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
